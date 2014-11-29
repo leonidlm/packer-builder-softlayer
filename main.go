0168973f08aae77a3b1c45afb4f183f7ca43af68
jamesdobson <dobson@softwarepunk.com>
2014-11-29 10:53:20 -0500

    Adding documentation for environment variables.

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/leonidlm/packer-builder-softlayer/builder/softlayer"
+	"github.com/jamesdobson/packer-builder-softlayer/builder/softlayer"
 	"github.com/mitchellh/packer/packer/plugin"
 )
 
